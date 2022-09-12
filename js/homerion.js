41de7003ae2621639dc12d3dc66f138a19d47a26
homerioni <homerion13@gmail.com>
2022-09-12 21:10:29 +0300

    up

--- js/homerion.js
+++ js/homerion.js
@@ -6,6 +6,12 @@ $(document).ready(function () {
         $('.modal-review').css('display', 'flex').hide().fadeIn();
     });
 
+    $('.photo-review__slide').click(function () {
+        $('.modal-review__text').text($(this).find('.photo-review__hover-text span').text());
+        $('body').addClass('lock');
+        $('.modal-review').css('display', 'flex').hide().fadeIn();
+    });
+
     $('.filter__btn').click(function () {
         $(this).parents('.filter__header').toggleClass('active');
         $(this).parents('.filter').find('.filter__body').slideToggle();
